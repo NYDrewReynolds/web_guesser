099c76036ee8f71647ee0a9756a9dce1cb2b8160
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-05-12 10:23:23 -0600

    Number guessing game complete. Background color changes & styling added

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(101)
+SECRET_NUMBER= rand(101)
+
+def check_guess(guess)
+  if guess == SECRET_NUMBER
+    "You got it right!"
+  elsif (guess - SECRET_NUMBER) > 5
+    message = "Way too high!"
+  elsif (SECRET_NUMBER - guess) > 5
+    message = "Way too low!"
+  elsif guess > SECRET_NUMBER
+    message = "Too high!"
+  elsif guess < SECRET_NUMBER
+    message = "Too low!"
+  end
+end
 
 get '/' do
-    erb :index, :locals => {:number => number}
+  guess = params["guess"].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :guess => guess}
 end
